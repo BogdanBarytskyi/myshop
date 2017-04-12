69c7e815437c108c66eb725fbbe54015abb55763
Maksim <maksimmysak@gmail.com>
2017-04-12 09:30:21 +0300

    add search Controller logic

--- app/Http/Controllers/SeachController.php
+++ app/Http/Controllers/SeachController.php
@@ -1,9 +1,15 @@
 <?php
 namespace App\Http\Controllers;
 use App\Http\Requests;
-class SeachController extends Controller{
-    public function index(){
+use App\Models\Product;
 
+class SeachController extends Controller{
+    public function index(Request $request){
+            $search = request->input('search');
+            $products = Product::where([['name','like','%'.$search.'%'],['active', '=', 1]])
+            ->orderBy('name')
+            ->paginate(16);
+        
         return view('seach');
     }
 }
